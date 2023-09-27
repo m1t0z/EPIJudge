5aaaacfd147bf55c2a6527d1e8deb0ae0184dee1
Dmitry Malyshev <damalyshev@gmail.com>
2023-09-27 08:57:26 +0300

    closet int with same weight

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -1,7 +1,25 @@
 #include "test_framework/generic_test.h"
+#include <climits>
+
+// Idea: iterate from LSB; find a pair of different neighbor bits; swap them.
 unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  
+  int width_in_bits = sizeof(x) * CHAR_BIT;
+  
+  for(int i = 0; i < width_in_bits - 1; ++i)
+  {
+    const auto b1 = i;
+    const auto b2 = i + 1;
+    if(((x >> b1) & 1) != ((x >> b2) & 1))
+    {
+      // Different neighbor bits found! Swap them!
+      const auto swap_mask = (1ull << b1) | (1ull << b2);
+      x ^= swap_mask;
+      break;
+    }
+  }
+
+  return x;
 }
 
 int main(int argc, char* argv[]) {
