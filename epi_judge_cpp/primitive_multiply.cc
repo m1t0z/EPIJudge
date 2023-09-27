e8ad103d594e97c46c64ac79d1ef4f1f95ff490e
Dmitry Malyshev <damalyshev@gmail.com>
2023-09-27 09:51:01 +0300

    primitive multiply

--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,7 +1,52 @@
 #include "test_framework/generic_test.h"
+#include <climits>
+
+unsigned long long BitwiseSum(unsigned long long x, unsigned long long y)
+{
+  const auto width_in_bits = sizeof(x) * CHAR_BIT;
+  
+  auto res = 0ull;
+  auto carry_bit = 0ull;
+  for(int i = 0; i < width_in_bits; ++i)
+  {
+    const auto b1 = (x >> i) & 1;
+    const auto b2 = (y >> i) & 1;
+
+    auto res_bit = b1 ^ b2 ^ carry_bit;
+    res |= (res_bit << i);
+
+    // Set the carry bit if we have 2+ ONES in the current bit.
+    carry_bit = b1&b2 | carry_bit&b1 | carry_bit&b2; 
+  }
+
+  if(carry_bit != 0)
+  {
+    throw std::overflow_error("Overflow!");
+  }
+
+  return res;
+}
+
+// Requirements:
+//  - allowed operators: =, ==, all bitwise
+//
+// Solution idea: use "multiplication by column" method with binary representation.
+//
 unsigned long long Multiply(unsigned long long x, unsigned long long y) {
-  // TODO - you fill in here.
-  return 0;
+  
+  auto res = 0ull;
+  while(y)
+  {
+    if(y&1)
+    {
+      res = BitwiseSum(res, x);
+    }
+
+    x <<= 1;
+    y >>= 1;
+  }
+
+  return res;
 }
 
 int main(int argc, char* argv[]) {
