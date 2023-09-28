74fad03a1a458b291ab3d54e97dab0b6ab1ee210
Dmitry Malyshev <damalyshev@gmail.com>
2023-09-28 09:57:34 +0300

    power

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,7 +1,33 @@
 #include "test_framework/generic_test.h"
+
+
+// Requirements:
+// - ignore underflow/overflow
 double Power(double x, int y) {
-  // TODO - you fill in here.
-  return 0.0;
+  
+  // Handle negative power.
+  if(y < 0)
+  {
+    x = 1.0/x;
+    y = -y;
+  }
+  
+  auto res = 1.0;
+  while(y)
+  {
+    if(y & 1)
+    {
+      res *= x;
+    }
+
+    // The main performance trick: instead of multiplying `x` the `y` number of times
+    // reduce the `y` and increase the x:
+    // - each reduce of `y` by 2 is equal to `x^2`
+    x *= x;
+    y >>= 1;
+  }
+
+  return res;
 }
 
 int main(int argc, char* argv[]) {
