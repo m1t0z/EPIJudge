4b70d0979767fda3b66a2fb8bd9eb6cf7a4ffa30
Dmitry Malyshev <damalyshev@gmail.com>
2023-09-28 09:34:06 +0300

    primitive divide

--- epi_judge_cpp/primitive_divide.cc
+++ epi_judge_cpp/primitive_divide.cc
@@ -1,7 +1,35 @@
 #include "test_framework/generic_test.h"
+
+
+// Requirements for allowed operators:
+// - addition
+// - subtraction
+// - shifting
+// Solution:
+// - to speedup calculations subtract in loop `2^k * y` from x and its remaining.
 int Divide(int x, int y) {
-  // TODO - you fill in here.
-  return 0;
+  
+  int res = 0;
+  
+  while(x >= y) {
+    int product = y;
+    int cnt_y = 1;
+    while(true)
+    {
+      auto new_product = static_cast<long long>(product) << 1;
+      if(new_product > x) 
+      {
+        break;
+      }
+      
+      product = new_product;
+      cnt_y <<= 1;
+    }
+    x -= product;
+    res += cnt_y;
+  }
+
+  return res;
 }
 
 int main(int argc, char* argv[]) {
